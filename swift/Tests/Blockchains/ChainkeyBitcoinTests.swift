401255b466998bb61c03994bdefc4b99ad0a9467
Ryan Croote <ryan.croote@dfinity.org>
2023-09-14 16:19:40 +0000

    Starting work on ios integration

--- swift/Tests/Blockchains/ChainkeyBitcoinTests.swift
+++ swift/Tests/Blockchains/ChainkeyBitcoinTests.swift
@@ -11,14 +11,12 @@ class ChainkeyBitcoinTests: XCTestCase {
     // TODO: Check and finalize implementation
 
     func testAddress() {
-        // TODO: Check and finalize implementation
+        let key = PrivateKey(data: Data(hexString: "ee42eaada903e20ef6e5069f0428d552475c1ea7ed940842da6448f6ef9d48e7")!)!
+        let pubkey = key.getPublicKeySecp256k1(compressed: false)
+        let address = AnyAddress(publicKey: pubkey, coin: .chainkeyBitcoin)
+        let addressFromString = AnyAddress(string: "iqlzk-yhdp6-7cb7b-zjtsb-anxbf-lv4uo-53aqu-r7xzl-cjizi-phwm4-3qe", coin: .chainkeyBitcoin)!
 
-        let key = PrivateKey(data: Data(hexString: "__PRIVATE_KEY_DATA__")!)!
-        let pubkey = key.getPublicKeyEd25519()
-        let address = AnyAddress(publicKey: pubkey, coin: .chainkeybitcoin)
-        let addressFromString = AnyAddress(string: "__ADDRESS_DATA__", coin: .chainkeybitcoin)!
-
-        XCTAssertEqual(pubkey.data.hexString, "__EXPECTED_PUBKEY_DATA__")
+        XCTAssertEqual(pubkey.data.hexString, "048542e6fb4b17d6dfcac3948fe412c00d626728815ee7cc70509603f1bc92128a6e7548f3432d6248bc49ff44a1e50f6389238468d17f7d7024de5be9b181dbc8")
         XCTAssertEqual(address.description, addressFromString.description)
     }
 
